108e954dea636e50eb6cb2e12cf0278df9b9a9e9
Tsumiki Miniwa <37684627+cpaca@users.noreply.github.com>
2026-01-18 15:40:05 -0800

    First DurationEstimator implementation

---
+++ src/client/java/com/incrementalclient/common/utils/DurationEstimator.java
@@ -0,0 +1,75 @@
+package com.incrementalclient.common.utils;
+
+import java.time.Duration;
+import java.time.Instant;
+
+/**
+ * Estimates durations - used by Skills to estimate time until a cooldown is ready or an active is over.
+ */
+public class DurationEstimator {
+
+    private Instant lastStart = null;
+    private Duration estimate;
+
+    public DurationEstimator(Duration defaultEstimate) {
+        this.estimate = defaultEstimate;
+    }
+
+    public DurationEstimator(int defaultEstimateSeconds) {
+        this(Duration.ofSeconds(defaultEstimateSeconds));
+    }
+
+    /**
+     * Creates a DurationEstimator with a default estimate of 0 seconds
+     */
+    public DurationEstimator() {
+        this(0);
+    }
+
+    /**
+     * Call when a new duration is started
+     */
+    public void start() {
+        lastStart = Instant.now();
+    }
+
+    /**
+     * Call when a new duration is stopped
+     * Effectively the same as saying "We estimate the duration will stop in 0 seconds".
+     */
+    public void stop() {
+       this.estimateStopsIn(Duration.ofSeconds(0));
+    }
+
+    /**
+     * Call when we estimate the duration to end after some time.
+     * @param futureEstimate How far in the future we estimate the duration to end in.
+     */
+    public void estimateStopsIn(Duration futureEstimate) {
+        if(this.lastStart == null) {
+            // Can't do any estimation with this
+            return;
+        }
+        var pastTime = Duration.between(this.lastStart, Instant.now());
+        // past + future = total estimated duration
+        this.estimate = pastTime.plus(futureEstimate);
+    }
+
+    /**
+     * Estimate the remaining amount of time in this duration.
+     * Will return null for negative values or if start() was never called.
+     * @return The amount of time left in this duration, or null if estimated duration passed, or if start() wasn't called.
+     */
+    public Duration getEstimatedRemainingDuration() {
+        if(this.lastStart == null) {
+            // Can't do any estimation with this.
+            return null;
+        }
+        var pastTime = Duration.between(this.lastStart, Instant.now());
+        var remainingDuration = estimate.minus(pastTime);
+        if (remainingDuration.isNegative()) {
+            return null;
+        }
+        return remainingDuration;
+    }
+}
