ba314803fb2495de69725623c8e63720ffe62c41
Jeremyrab <29066462+Jeremyrab@users.noreply.github.com>
2026-01-22 00:11:26 +1100

    Change how it is displayed and the enum selector for tt skip

--- src/client/java/com/incrementalclient/featues/Tasking/TaskingOverrides.java
+++ src/client/java/com/incrementalclient/featues/Tasking/TaskingOverrides.java
@@ -2,23 +2,24 @@ package com.incrementalclient.featues.Tasking;
 
 import com.google.common.base.Suppliers;
 import com.incrementalclient.abstractions.ListenableBase;
-import com.incrementalclient.common.data.tasks.Constraint;
-import com.incrementalclient.common.data.tasks.Task;
 import com.incrementalclient.common.data.Tool;
+import com.incrementalclient.common.data.tasks.Task;
 import com.incrementalclient.common.utils.TextUtils;
-import com.incrementalclient.config.controllers.ComplexTypeController;
 import com.incrementalclient.config.InsertableListOption;
+import com.incrementalclient.config.controllers.ComplexTypeController;
 import com.incrementalclient.interfaces.ComplexConfigurable;
 import com.incrementalclient.interfaces.Listener;
 import com.incrementalclient.internals.MinecraftClientAccessor;
-import com.incrementalclient.featues.Tasking.TicketTaskOverride;
-import dev.isxander.yacl3.api.*;
-import dev.isxander.yacl3.api.controller.BooleanControllerBuilder;
+import dev.isxander.yacl3.api.ConfigCategory;
+import dev.isxander.yacl3.api.Option;
+import dev.isxander.yacl3.api.OptionDescription;
+import dev.isxander.yacl3.api.YetAnotherConfigLib;
+import dev.isxander.yacl3.api.controller.EnumControllerBuilder;
 import dev.isxander.yacl3.api.controller.EnumDropdownControllerBuilder;
-import dev.isxander.yacl3.api.controller.IntegerSliderControllerBuilder;
 import dev.isxander.yacl3.api.controller.StringControllerBuilder;
 import dev.isxander.yacl3.config.v2.api.SerialEntry;
 import net.minecraft.text.Text;
+import net.minecraft.text.MutableText;
 
 import java.util.HashMap;
 import java.util.List;
@@ -39,66 +40,67 @@ public class TaskingOverrides extends ListenableBase<Listener> implements Comple
         this.screenAccessor = screenAccessor;
     }
 
-    private List<OptionPiece> createScreen(){
+    private List<OptionPiece> createScreen() {
         return List.of(Categories.Tasking.createConfig(10000,
                 InsertableListOption.<Configuration.Override>createBuilder()
-                .name(Text.literal("Task Specific Overrides"))
-                .binding(
-                        configuration.overrides,
-                        () -> configuration.overrides,
-                        v -> configuration.overrides = v
-                )
-                .description(OptionDescription.of(Text.of("Options to override the default behaviour per task")))
-                .insertEntriesAtEnd(false)
-                .customController(o -> ComplexTypeController.create(o, screenAccessor)
-                        .textProvider(opt -> (opt.task == null ? Text.of("Invalid Task Override") : getTextProvider(opt)))
-                        .screenFactory(opt -> YetAnotherConfigLib.createBuilder()
-                                .title(Text.of("Edit Override Settings"))
-                                .category(ConfigCategory.createBuilder()
-                                        .name(Text.of("Overrides"))
-                                        .option(Option.<Task>createBuilder()
-                                                .name(Text.of("Task"))
-                                                .binding(opt.task, () -> opt.task, val -> opt.task = val)
-                                                .controller(t -> EnumDropdownControllerBuilder.create(t)
-                                                        .formatValue(v -> Text.of(v.getDescriptor().displayName()))
-                                                )
+                        .name(Text.literal("Task Specific Overrides"))
+                        .binding(
+                                configuration.overrides,
+                                () -> configuration.overrides,
+                                v -> configuration.overrides = v
+                        )
+                        .description(OptionDescription.of(Text.of("Options to override the default behaviour per task")))
+                        .insertEntriesAtEnd(false)
+                        .customController(o -> ComplexTypeController.create(o, screenAccessor)
+                                .textProvider(opt -> (opt.task == null ? Text.of("Invalid Task Override") : getTextProvider(opt)))
+                                .screenFactory(opt -> YetAnotherConfigLib.createBuilder()
+                                        .title(Text.of("Edit Override Settings"))
+                                        .category(ConfigCategory.createBuilder()
+                                                .name(Text.of("Overrides"))
+                                                .option(Option.<Task>createBuilder()
+                                                        .name(Text.of("Task"))
+                                                        .binding(opt.task, () -> opt.task, val -> opt.task = val)
+                                                        .controller(t -> EnumDropdownControllerBuilder.create(t)
+                                                                .formatValue(v -> Text.of(v.getDescriptor().displayName()))
+                                                        )
+                                                        .build())
+                                                .option(Option.<String>createBuilder()
+                                                        .name(Text.of("Warp"))
+                                                        .binding(opt.warp, () -> opt.warp, val -> opt.warp = val)
+                                                        .controller(StringControllerBuilder::create)
+                                                        .build())
+                                                .option(Option.<String>createBuilder()
+                                                        .name(Text.of("Wardrobe"))
+                                                        .binding(opt.wardrobe, () -> opt.wardrobe, val -> opt.wardrobe = val)
+                                                        .controller(StringControllerBuilder::create)
+                                                        .build())
+                                                .option(Option.<String>createBuilder()
+                                                        .name(Text.of("Pet"))
+                                                        .binding(opt.pet, () -> opt.pet, val -> opt.pet = val)
+                                                        .controller(StringControllerBuilder::create)
+                                                        .build())
+                                                .option(Option.<Tool>createBuilder()
+                                                        .name(Text.of("Tool"))
+                                                        .binding(opt.tool, () -> opt.tool, val -> opt.tool = val)
+                                                        .controller(t -> EnumDropdownControllerBuilder.create(t)
+                                                                .formatValue(v -> Text.of(v.name()))
+                                                        )
+                                                        .build())
+                                                .option(Option.<TicketTaskOverride>createBuilder()
+                                                        .name(Text.of("Ticket task Skip Behavior"))
+                                                        .binding(opt.skipTicket, () -> opt.skipTicket, val -> opt.skipTicket = val)
+                                                        .controller(t -> EnumControllerBuilder.create(t)
+                                                                .enumClass(TicketTaskOverride.class)
+                                                                .formatValue(v -> Text.of(v.getName()))
+                                                        )
+                                                        .build())
                                                 .build())
-                                        .option(Option.<String>createBuilder()
-                                                .name(Text.of("Warp"))
-                                                .binding(opt.warp, () -> opt.warp, val -> opt.warp = val)
-                                                .controller(StringControllerBuilder::create)
-                                                .build())
-                                        .option(Option.<String>createBuilder()
-                                                .name(Text.of("Wardrobe"))
-                                                .binding(opt.wardrobe, () -> opt.wardrobe, val -> opt.wardrobe = val)
-                                                .controller(StringControllerBuilder::create)
-                                                .build())
-                                        .option(Option.<String>createBuilder()
-                                                .name(Text.of("Pet"))
-                                                .binding(opt.pet, () -> opt.pet, val -> opt.pet = val)
-                                                .controller(StringControllerBuilder::create)
-                                                .build())
-                                        .option(Option.<Tool>createBuilder()
-                                                .name(Text.of("Tool"))
-                                                .binding(opt.tool, () -> opt.tool, val -> opt.tool = val)
-                                                .controller(t -> EnumDropdownControllerBuilder.create(t)
-                                                        .formatValue(v -> Text.of(v.name()))
-                                                )
-                                                .build())
-                                        .option(Option.<TicketTaskOverride>createBuilder()
-                                                .name(Text.of("Ticket task Skip Behavior"))
-                                                .binding(opt.skipTicket, () -> opt.skipTicket, val -> opt.skipTicket = val)
-                                                .controller(t -> EnumDropdownControllerBuilder.create(t)
-                                                        .formatValue(v -> Text.of(v.name()))
-                                                )
-                                                .build())
-                                        .build())
-                                .save(this::notifyListeners))
+                                        .save(this::notifyListeners))
+                                .build()
+                        )
+                        .initial(Configuration.Override::new)
+                        .collapsed(true)
                         .build()
-                )
-                .initial(Configuration.Override::new)
-                .collapsed(true)
-                .build()
         ));
     }
 
@@ -116,7 +118,7 @@ public class TaskingOverrides extends ListenableBase<Listener> implements Comple
         return configuration;
     }
 
-    public HashMap<Task, Configuration.Override> getOverrides(){
+    public HashMap<Task, Configuration.Override> getOverrides() {
         return overrides;
     }
 
@@ -131,7 +133,7 @@ public class TaskingOverrides extends ListenableBase<Listener> implements Comple
                 .collect(Collectors.toMap(
                         o -> o.task,
                         o -> o,
-                        (existing, replacement)-> existing,
+                        (existing, replacement) -> existing,
                         HashMap::new
                 ));
     }
@@ -141,7 +143,7 @@ public class TaskingOverrides extends ListenableBase<Listener> implements Comple
         @SerialEntry
         public List<Override> overrides = new java.util.ArrayList<>();
 
-        public static class Override{
+        public static class Override {
             @SerialEntry
             public Task task = Task.SellItems;
             @SerialEntry
@@ -158,16 +160,24 @@ public class TaskingOverrides extends ListenableBase<Listener> implements Comple
     }
 
     private Text getTextProvider(Configuration.Override override) {
-        int overrideColor = switch (override.skipTicket) {
-            case TicketTaskOverride.Default -> 0xFFFFFF;
-            case TicketTaskOverride.Skipped -> 0xf698ff;
-            case TicketTaskOverride.NotSkipped -> 0xff9898;
-        };
-        return TextUtils.textColor(override.task.getDescriptor().displayName() +
-                (override.wardrobe.isEmpty() ? "" : ", Wardrobe: " + override.wardrobe) +
-                (override.warp.isEmpty() ? "" : ", Warp: " + override.warp) +
-                (override.pet.isEmpty() ? "" : ", Pet: " + override.pet) +
-                (override.tool == Tool.Default ? "" : ", Tool: " + override.tool.name()),
-                overrideColor);
+        MutableText textProvider = Text.literal("");
+
+        // Figure out better colors
+        getConditionText(textProvider, true, "Task", override.task.getDescriptor().displayName(), 0xFA3B1, 0x12c4d5);
+        getConditionText(textProvider, !override.wardrobe.isEmpty(), "Wardrobe", override.wardrobe, 0x8C351A, 0xb04221);
+        getConditionText(textProvider, !override.warp.isEmpty(), "Warp", override.warp, 0x009A21, 0x00be29);
+        getConditionText(textProvider, !override.pet.isEmpty(), "Pet", override.pet, 0x949F00, 0xb5c300);
+        getConditionText(textProvider, override.tool != Tool.Default, "Tool", override.tool.name(), 0x7F8593,0x9da5b7);
+        getConditionText(textProvider, override.skipTicket != TicketTaskOverride.Default, "TT", override.skipTicket.getName(), 0x8845D1, 0x9e50f5);
+
+        return textProvider;
     }
+
+    private void getConditionText(MutableText textProvider, boolean condition, String category, String valueIfTrue, int colorText, int colorValue) {
+        if (condition) {
+            textProvider.append(TextUtils.textColor(category + ": ", colorText));
+            textProvider.append(TextUtils.textColor(valueIfTrue + " ", colorValue));
+        }
+    }
+
 }
