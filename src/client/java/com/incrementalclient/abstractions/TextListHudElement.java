2a39a6e77aded2966ee46f4834730afa0105b8ff
Jeremyrab <29066462+Jeremyrab@users.noreply.github.com>
2026-01-24 03:38:07 +1100

    Make TextListHudElement bounding box scale

--- src/client/java/com/incrementalclient/abstractions/TextListHudElement.java
+++ src/client/java/com/incrementalclient/abstractions/TextListHudElement.java
@@ -126,7 +126,7 @@ public abstract class TextListHudElement<T extends TextListHudElement.Configurat
                 .orElse(0);
         int height = HudConstants.TEXT_PADDING_Y + (HudConstants.LINE_SPACING * texts.size()) + 2; // +2 for bottom padding
 
-        return new Vector2f(maxWidth + HudConstants.BACKGROUND_PADDING, height);
+        return new Vector2f((maxWidth + HudConstants.BACKGROUND_PADDING) * scale, height * scale);
     }
 }
 
